0c20e98536b2bb51e6537196fa995dd15fb5202f
SofiyanKhan7x <suffu7x@gmail.com>
2025-04-13 17:50:00 +0530

    lets get started building

---
+++ src/main/java/com/suffu/ProductNest_Api/ProductNestApiApplication.java
@@ -0,0 +1,13 @@
+package com.suffu.ProductNest_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductNestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductNestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suffu/ProductNest_Api/ProductNestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suffu.ProductNest_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductNestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
