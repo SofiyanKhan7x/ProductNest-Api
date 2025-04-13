e69cffcc1ee8453fe51c6f0e0eba30870060a384
SofiyanKhan7x <suffu7x@gmail.com>
2025-04-13 19:59:49 +0530

    PUT and DELETE Features added

--- src/main/java/com/suffu/ProductNest_Api/controller/ProductController.java
+++ src/main/java/com/suffu/ProductNest_Api/controller/ProductController.java
@@ -12,24 +12,40 @@ public class ProductController {
     @Autowired
     ProductService service;
 
+    //For Home Page
     @RequestMapping("/")
     public String greet(){
         return "Welcome to Project";
     }
 
+    //Fetching all products from the h2 database
     @GetMapping("/products")
     public List<Product> getProduct(Product product){
         return service.getProduct(product);
     }
 
+    //Fetching all products by id from the h2 database
     @GetMapping("/products/{productId}")
     public Product getProductById(@PathVariable int productId){
         return service.getProductById(productId);
     }
 
+    //Posting products and storing in the h2 database
     @PostMapping("/products")
     public void addProduct(@RequestBody  Product product){
         System.out.println("Product Added");
          service.addProduct(product);
+    }
+
+    //Put for updating the data in the h2 database
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product product){
+        service.updateProduct(product);
+    }
+
+    //Delete product from the database
+    @DeleteMapping("/products/{productId}")
+    public void deleteProduct(@PathVariable int productId){
+        service.deleteProduct(productId);
     }
 }
--- src/main/java/com/suffu/ProductNest_Api/service/ProductService.java
+++ src/main/java/com/suffu/ProductNest_Api/service/ProductService.java
@@ -13,15 +13,28 @@ public class ProductService {
     @Autowired
     ProductRepository repo;
 
+    //fetching and returning the list of products
     public List<Product> getProduct(Product product){
         return repo.findAll();
     }
 
+    //fetching product from database through product Id
     public Product getProductById(int productId){
         return repo.findById(productId).orElse(new Product());
     }
 
+    //add product in the database
     public void addProduct(Product product){
         repo.save(product);
+    }
+
+    //update the product in the database
+    public void updateProduct(Product product){
+        repo.save(product);
+    }
+
+    //Deleting the product in the database
+    public  void deleteProduct(int productId){
+        repo.deleteById(productId);
     }
 }
